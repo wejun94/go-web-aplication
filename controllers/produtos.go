b4c9f3ed3c500df621087ae4599a199a4c5d2a57
wejun94 <wellingtonjunior208@hotmail.com>
2025-05-30 20:12:01 -0500

    salvando e inserindo dados no banco de dados

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,14 +2,47 @@ package controllers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 	"web-application/models"
 )
 
-var temp = template.Must(template.ParseGlob("templates/*index.html"))
+var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	todosOsProdutos := models.BuscarTodosOsProdutos()
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	//rmazenando na base de dados
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		//convertendo preço string para float
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço:", err)
+		}
+
+		//convertendo preço string para inteiro
+		quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade:", err)
+		}
+
+		models.CriaNovoProduto(nome, descricao, precoConvertido, quantidadeConvertidaParaInt)
+	}
+
+	http.Redirect(w, r, "/", 301)
 
 }
